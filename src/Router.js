ed91ca750b138565bd24b9e1ee16e3736189d992
Simon <simon@radinn.com>
2023-02-24 11:10:51 +0100

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ViewPropTypes, BackHandler, Linking } from 'react-native';
+import { BackHandler, Linking } from 'react-native';
 import PropTypes from 'prop-types';
 import NavigationStore from './Store';
 import defaultStore from './defaultStore';
@@ -132,7 +132,6 @@ Router.propTypes = {
   navigator: PropTypes.func,
   wrapBy: PropTypes.func,
   getSceneStyle: PropTypes.func,
-  sceneStyle: ViewPropTypes.style,
   createReducer: PropTypes.func,
   children: PropTypes.element,
   uriPrefix: PropTypes.string,
